19abe369a868c4112a0314211267bf82447ad18e
SamVargas <samuel.vargas02@uptc.edu.co>
2024-09-08 21:57:29 -0500

    Update_View

--- src/views/CreateAppointment.vue
+++ src/views/CreateAppointment.vue
@@ -77,7 +77,7 @@ export default {
   max-width: 600px;
   margin: 0 auto;
   padding: 20px;
-  background: #bffbff; 
+  background: #e5e5e5; /* Fondo aplicado */
   border-radius: 8px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
